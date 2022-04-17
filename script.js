21e0bff137d89fa62a8f5bb067ae020d6ae41287
jieunhong <hong.bbubbu@gmail.com>
2022-04-17 19:52:29 +0900

    과제 파일 커밋

---
+++ script.js
@@ -0,0 +1,346 @@
+
+const textFile = [
+    {
+        "script": [
+            {
+                "text": "안녕! 나는 건국대학교 신산업융합학과 202111536 홍지은이라고 해.",
+                "icon": "normal"
+            },
+            {
+                "text": "만나서 반가워.",
+                "icon": "eye"
+            },
+            {
+                "text": "이 게임은 처음이니?",
+                "icon": "talk"
+            },
+            {
+                "text": "그냥 네가 알던 시뮬레이션 게임과 똑같아",
+                "icon": "talk"
+            },
+            {
+                "text": "다만, 조금 더 허술하면서도..",
+                "icon": "talk"
+            },
+            {
+                "text": "내 게임과 애니메이션 일대기를 알 수 있지",
+                "icon": "talk"
+            },
+            {
+                "text": "너는 그저 약간의 질문을 맞추기만 하면 돼.",
+                "icon": "talk"
+            },
+            {
+                "text": "자 그럼 첫 번째 질문! 내가 처음으로 게임에 관심을 갖게된 경로는 뭘까?",
+                "icon": "normal"
+            },
+        ],
+        "question": [
+            "가족",
+            "친구",
+            "학교"
+        ],
+        "answer": 0,
+
+    },
+    {
+        "script": [
+            {
+                "text": "좋아! 아빠가 게임을 좋아하셔서 자연스럽게 언니랑 나도 어릴 적 부터 게임을 접하게 됐어.",
+                "icon": "fighting"
+            },
+            {
+                "text": "중학교 3학년때였어. 그 날도 평화롭게 게임을 즐기고 있었지",
+                "icon": "talk"
+            },
+            {
+                "text": "[그 사건]이 일어나기 전에는 말이야.",
+                "icon": "hmm"
+            },
+            {
+                "text": "당시 운영진의 공지는 게임 화면 상단에 나오고 몇 초 뒤에 사라지는 방싱이었어.",
+                "icon": "talk"
+            },
+            {
+                "text": "평소처럼 패치 내용 관련한 공지가 나오고 있었는데.. 한 운영진 분의 실수로 명령어가 공지로 유출된거야",
+                "icon": "talk"
+            },
+            {
+                "text": "플레이하던 모든 유저들이 그걸 목격했고.. 곧 게임은 난장판으로 변했어",
+                "icon": "sad"
+            },
+            {
+                "text": "사람들은 끊임없이 명령어를 이용해 공지를 남발했고, 초단위로 바뀌는 공지와 폭주한 유저들의 채팅으로 인해 게임 내에서도 트래픽을 감당하지 못해 렉이 걸려 정상적인 플레이가 불가능한 상황이 됐지.",
+                "icon": "sad"
+            },
+            {
+                "text": "곧 긴급점검을 한다는 말과 함께 게임은 그대로 접속 종료가 되었어.",
+                "icon": "sad"
+            },
+            {
+                "text": "단순히 게임을 하는 행위에만 집중하던 내가 게임이 어떻게 만들어지고 동작하는지에 대해 관심을 갖게 된 결정적 계기가 되었지",
+                "icon": "wow"
+            },
+            {
+                "text": "게임 프로그래머가 되기 위한 첫 걸음으로 IT계열 마이스터고에 진학을 했어.",
+                "icon": "talk"
+            },
+            {
+                "text": "역시 프로그래밍 전공인지 다들 각자 특화된 분야가 있었지. 나는 그 중에서도 방과후 시간에 빠르게 문제를 풀고 몰래 메이플스토리를 하는 트리오 중에 한 명이었어.",
+                "icon": "talk"
+            },
+            {
+                "text": "입학을 하고 보니, 우리 학교는 게임 프로그래밍을 위한 커리큘럼은 존재하지 않았어. 하지만 딱 1번 기회가 생겼지.",
+                "icon": "talk"
+            },
+            {
+                "text": "기능경기대회에 게임개발 종목에 참가하는 동아리 특강을 같이 들을 수 있었던거야! 학교를 나오지 않아도 되는 꿀같은 방학에 진행된다는 점이 슬프긴 했지만, 망설임 없이 신청하게 되었어",
+                "icon": "talk"
+            },
+            {
+                "text": "하지만 나는 이 강의 이후로 게임 개발자의 꿈을 접게 돼",
+                "icon": "sad"
+            },
+            {
+                "text": "나를 좌절하게 만든 이 게임개발 특강은 어떤 프로그래밍 언어로 진행되었을까?",
+                "icon": "hmm"
+            },
+        ],
+        "question": [
+            "JAVA",
+            "HTML",
+            "C#",
+            "Python"
+        ],
+        "answer": 2,
+    },
+
+    {
+        "script": [
+            {
+                "text": "혹시 개발 좀 하는 친구야? 맞아 그 경기는 C#으로 진행되기 때문에, 동일한 언어로 특강이 진행되었지.",
+                "icon": "wow"
+            },
+            {
+                "text": "이제 막 화면에 Hello World! 하고 글자를 찍던 새내기에게 현란한 그래픽과 로직, 수식이 필요한 게임 개발의 산은 알프스산맥처럼 견고하고 높아보였어. 지금 그 개발 코드를 본다면 강의를 듣지 않아도 이해할 수 있겠지만, 그 당시에는 나에게 굉장한 좌절감을 안겨주었어.",
+                "icon": "sad"
+            },
+            {
+                "text": "좋아하는 것은 일로 하는 것보다 취미로 즐기는 것이 더 좋다는 걸 깨닫게 되었지. 이후에도 종종 친구들과 콘솔창에 간단한 조건문으로 이루어진 텍스트 시뮬레이션 게임을 만들면서 놀기도 헸어.",
+                "icon": "idea"
+            },
+            {
+                "text": "맞아 지금 이 게임의 베이스가 된 게임이지.",
+                "icon": "talk"
+            },
+            {
+                "text": "어느정도 학교에 적응을 하고나자 피끓는 청춘..! 다들 연애를 하고 싶어했지.",
+                "icon": "talk"
+            },
+            {
+                "text": "전공은 공학계열이지만, 우리 학교는 여고라 또래의 남자친구들을 접할 기회가 많이 없었어",
+                "icon": "talk"
+            },
+            {
+                "text": "결국 2D의 세계로 빠지게 되는데..!?",
+                "icon": "hmm"
+            },
+            {
+                "text": "애니메이션도 어릴 적 부터 좋아하던 편이어서 중학교때는 애니메이션부 동아리 부장으로 활동하면서 한국의 유명한 '코믹월드'에 부원들과 부스를 빌려서 직접 굿즈를 제작해서 팔기도 했지.",
+                "icon": "normal"
+            },
+            {
+                "text": "고등학교에 들어와서는 '러브코미디', '학원', '청춘' 키워드가 붙어있는 애니메이션을 종종 보곤 했어",
+                "icon": "normal"
+            },
+            {
+                "text": "어느 날, 평소처럼 애니메이션을 감상하던 내 뒷모습을 친구들이 찍어서 반 단체 카카오톡 방에 공유하던 일이 있었어.",
+                "icon": "normal"
+            },
+            {
+                "text": "애니메이션의 그림체와 결합된 내 모습을 보고 친구들은 '오타쿠'라면서 장난을 쳤고, 그 장난은 지금까지도 종종 이어지고 있어.",
+                "icon": "sad"
+            },
+            {
+                "text": "우리학교에도 애니메이션을 즐기는 친구들은 매우 많고 드문 모습이 전혀 아니었지만, 단순히 친구들끼리의 장난을 위한 목적으로 그런 일이 일어났던 거야.",
+                "icon": "sad"
+            },
+            {
+                "text": "생각보다 그 장난은 내게 큰 상처로 다가왔고, 고3 이후로 일본 애니메이션은 거의 보지 않아. 대신 좀 더 상상의 나래를 펼칠 수 있는 책에 관심을 갖게 되었지.",
+                "icon": "fighting"
+            },
+            {
+                "text": "이때, 놀림을 받은 사진 속에서 내가 보고있던 애니메이션의 제목은 뭘까?",
+                "icon": "normal"
+            },
+        ],
+        "question": [
+            "월간순정 노자키군",
+            "진격의 거인",
+            "너에게 닿기를",
+            "코드기어스"
+        ],
+        "answer": 0,
+    },
+    {
+        "script": [
+            {
+                "text": "혹시 미림여자정보과학고등학교 출신은 아니지? 적나라한 제목이지만 꼭 보길 추천해! 굉장히 재미있는 애니메이션이야. 사실 지금도 가끔씩 생각나면 찾아보곤 해.",
+                "icon": "wow"
+            },
+            {
+                "text": "이외에도 디즈니, 픽사의 영화를 종종 보긴 하지만 3D 애니메이션은 가끔 '불쾌한 골짜기'심리가 나타날 때가 있지.",
+                "icon": "talk"
+            },
+            {
+                "text": "대망의 졸업을 하고! 나는 웹 개발자로 사회에 첫 발을 내딛게 되었어.",
+                "icon": "success"
+            },
+            {
+                "text": "사회초년생의 서투른 사회생활에서 만나게 된 책임감과 소통의 문제는 많은 스트레스를 야기했고",
+                "icon": "sad"
+            },
+            {
+                "text": "청소년이라는 보호막에서 벗어난 갓 성인이 된 아이들은 삼삼오오 모여 스트레스 해소를 핑계로 음주가무를 일삼게 되었지.",
+                "icon": "talk"
+            },
+            {
+                "text": "회사에 적응이 된 이후에는 이전처럼 음주를 즐기진 않지만, 바쁜 회사 일정과 피로도로 인해 꾸준한 게임과 애니메이션 감상 대신에 빠르게 읽을 수 있는 책에 좀 더 집중을 하게 되었어.",
+                "icon": "talk"
+            },
+            {
+                "text": "고전 문학으로 시작해서 비문학, 웹소설까지 도달하자 이제 책 이외에 다른 미디어가 필요하더라고.(그 와중에도 자기개발서는 싫더라)",
+                "icon": "talk"
+            },
+            {
+                "text": "확실히 이 취미를 다시 갖게되니까 집에 가는 길이 두근두근해! 이 취미는 뭘까?",
+                "icon": "eye"
+            },
+        ],
+        "question": [
+            "애니메이션",
+            "개인 프로젝트 개발",
+            "게임"
+        ],
+        "answer": 2,
+    },
+    {
+        "script": [
+            {
+                "text": "공부가 아니라 아쉽지만 게임이 내 퇴근길을 두근두근하게 만들어주었어.",
+                "icon": "success"
+            },
+            {
+                "text": "주로 '리그오브레전드'라는 AOS게임과 Steam사에서 퍼블리싱하는 게임들 그리고 닌텐도 스위치의 '동물의 숲'이 내 저녁을 알차게 만들어주고 있지.",
+                "icon": "talk"
+            },
+            {
+                "text": "성인이 되고 새로운 사람들을 만나는 상황에서 게임이라는 대화 주제는 초면인 사람들도 10년지기 친구처럼 만들어주는 마법의 단어야!",
+                "icon": "talk"
+            },
+            {
+                "text": "실제로 우연하게 게임을 같이 하게된 사람과 지금까지도 종종 저녁 후의 식사도 같이 하고 만나서 놀러다니기도 하는 친밀한 관계가 된 경험도 있고,",
+                "icon": "talk"
+            },
+            {
+                "text": "코로나로 인해서 서로 어색한 동기들과 편하게 이야기를 나누고 서로를 알아갈 수 있는 계기가 되기도 했어.",
+                "icon": "eye"
+            },
+            {
+                "text": "내 삶의 게임과 애니메이션에 대한 이야기는 이제 끝이야! 이후는 아직 미래의 일이라 나도 알 수 없거든",
+                "icon": "talk"
+            },
+            {
+                "text": "게임과 애니메이션이 내 삶에 큰 영향을 주었다는건 부정할 수 없을정도로 확실해. 난 서버 개발자로 일하고 있는 지금도 주어진 업무=퀘스트, 월급=퀘스트 보상, 연차나 실력=레벨과 스탯이라고 생각하며 열심히 레벨 업 중인걸?",
+                "icon": "talk"
+            },
+            {
+                "text": "가끔 힘든 일이 오거나, 우울할 때에는 일부러 애니메이션에 나오는 주인공들처럼 행동하려고 노력해. 행복하다고 생각하면서 살아가니까 정말 행복한 사람으로 만들어주었어.",
+                "icon": "sad"
+            },
+            {
+                "text": "사실 지금 내 삶도, 이 게임을 플레이 하는 네 삶도 우리가 알지 못하는 다른 곳에선 한 편의 애니메이션 또는 인생 시뮬레이션 게임이 아닐까?",
+                "icon": "fighting"
+            },
+            {
+                "text": "이 게임에도 배드엔딩과 해피엔딩이 있지만, 결국 다시 시도하고 노력하면 해피엔딩을 볼 수 있는 것처럼! 열심히 퀘스트를 깨고 업적을 이뤄나가야지. 랭커가 되는 그 날 까지!",
+                "icon": "fighting"
+            },
+
+            {
+                "text": "이상으로 건국대학교 신산업융합학과 202111536 홍지은 게임과 애니메이션 과제였습니다. 감사합니다.",
+                "icon": "fighting"
+            },
+        ],
+        "question": null,
+        "answer": 2,
+    }
+
+]
+const textArea = document.querySelector(".typing");
+const btnArea = document.querySelector(".select-btn-wrap ");
+var count = 0;
+let i = 0;
+let j = 0;
+window.onload = function () {
+    let selectBtns = document.getElementsByClassName("select-btn");
+    for (var x = 0; x < selectBtns.length; x++) {
+        selectBtns[x].addEventListener("click", function () {
+            if (this.attributes.value.value == textFile[j].answer) {
+                j++;
+                i=0;
+                count= 0;
+                for (var index = 0; index < btnArea.children.length; index++) {
+                    if (!btnArea.children[index].classList.value.includes("dp-none"))
+                        btnArea.children[index].classList += " dp-none";
+                }
+                setTimeout(typing, 100);
+            } else {
+                location.href = "bad.html";
+            }
+        })
+    }
+
+    setTimeout(typing, 100)
+
+};
+function typing() {
+    let text = textFile[j].script[count].text;
+    if (i == 0) {
+        switch (textFile[j].script[count].icon) {
+            case "hmm": document.querySelector(".main_img").src = "hmm.png";
+            break;
+            case "fighting": document.querySelector(".main_img").src = "fighting.png"; break;
+            case "eye": document.querySelector(".main_img").src = "eye.png";break;
+            case "sad": document.querySelector(".main_img").src = "sad.png";break;
+            case "wow": document.querySelector(".main_img").src = "wow.png";break;
+            case "success": document.querySelector(".main_img").src = "success.png";break;
+            case "talk": document.querySelector(".main_img").src = "talk.png";break;
+            case "idea": document.querySelector(".main_img").src = "idea.png";break;
+            default:document.querySelector(".main_img").src = "1.png";
+        }
+        textArea.innerHTML = "";
+    }
+    if (i < text.length) {
+        let txt = text.charAt(i);
+        textArea.innerHTML += txt;
+        i++;
+        setTimeout(typing, 50)
+    } else {
+        if (count < textFile[j].script.length - 1) {
+            i = 0;
+            count++;
+            setTimeout(typing, 500);
+        } else {
+            if(textFile[j].question == null){
+                setTimeout(location.href="happy.html", 1000);
+            }
+            textFile[j].question.forEach(function (element, index, array) {
+                btnArea.children[index].textContent = element;
+                if(textFile[j].answer == index) btnArea.children[index].textContent +="(정답)";
+                btnArea.children[index].classList.remove("dp-none");
+            });
+
+        }
+    }
+}
